8f0159a420bf39ca10cccccbb23f9b030aaecda9
enZondi <4346549@myuwc.ac.za>
2025-05-30 03:56:13 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory, jsonify
+from app.ivanti import fetch_incidents
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("index.html", message="🧠 ThinkTank CLI Container is Live!")
+def root_index():
+    return send_from_directory(".", "index.html")
+
+@app.route("/api/incidents")
+def get_incidents():
+    data = fetch_incidents()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
