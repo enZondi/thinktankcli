9b49717da9215b62bcb0eb55c0c030fe815a7c10
enZondi <4346549@myuwc.ac.za>
2025-05-30 12:07:25 +0200

    Update ivanti_client.py

--- app/ivanti_client.py
+++ app/ivanti_client.py
@@ -1,6 +1,7 @@
 # app/ivanti_client.py
 import requests
-from config import IVANTI_API_KEY
+import os
+IVANTI_API_KEY = os.environ.get('IVANTI_API_KEY')
 
 def fetch_ivanti_incidents():
     url = "https://trainer.thinktanks.co.za/HEAT/api/odata/businessobject/incidents"
